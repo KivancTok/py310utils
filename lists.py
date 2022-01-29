8bc4af060e21e01ee951c39a8023787d4a2e68d0
Techsplosion <67820127+KivancTok@users.noreply.github.com>
2022-01-29 22:29:50 +0300

    Delete lists.py

--- lists.py
+++
@@ -1,8 +0,0 @@
-def search(search_list: list, value):
-    for i in range(len(search_list)):
-        if search_list[i] == value:
-            print(f"Item found! Index: {i}")
-            break
-
-    else:
-        print("Not found.")
